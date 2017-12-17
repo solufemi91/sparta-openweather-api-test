237ab571c131940acee5f6c129dc32c5bdd1c8bd
solufemi91 <stevenoo@spartaglobal.com>
2017-12-17 14:45:06 +0000

    added code to check whether a key existed or not in the code

--- lib/services/single/by_city_name_service.rb
+++ lib/services/single/by_city_name_service.rb
@@ -159,5 +159,6 @@ end
 
 
 x = GetWeatherByCityName.new
- puts x.get_weather_by_city_name("Abuja")
-puts x.get_sys_sunset
+ # puts x.get_weather_by_city_name("Abuja")
+ # puts x.get_weather_by_city_name("London")
+# puts x.get_cod
